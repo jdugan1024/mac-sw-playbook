23f0660c6a55e70b33395434cbea5447e52bc94b
Jon Dugan <jdugan@x1024.net>
2017-03-29 23:41:09 -0500

    Update bootstrap.sh; add extra casks

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,5 @@
 
 # shell script to bootstrap the system to the point that it has ansible
 
-echo Installing XCode
-
-xcode-select --install
-read -p "[Once XCode is installed hit RETURN to continue]"
 sudo easy_install pip
 sudo pip install ansible
